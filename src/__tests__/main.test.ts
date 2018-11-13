ee4df9ffb3cc56e349ba0d3c37e69592ac1c80b3
mizchi <miz404@gmail.com>
2018-11-13 20:42:30 +0900

    Fix test example

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,10 +1,16 @@
 import assert from "assert";
 
-beforeEach(() => {
-  document.body.innerHTML = `<div class="root"></div>`;
+test("sample", () => {
+  assert(1 === 1);
 });
 
-test("Run app", () => {
-  require("../main");
-  assert(document.body.textContent === "Hello, {app_name}");
-});
+// === Test example
+//
+// beforeEach(() => {
+//   document.body.innerHTML = `<div class="root"></div>`;
+// });
+
+// test("Run app", () => {
+//   require("../main");
+//   assert(document.body.textContent === "Hello, {app_name}");
+// });
