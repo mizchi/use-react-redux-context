f57ab56897c09fe0219467beadcc05d4a1d4d86e
mizchi <miz404@gmail.com>
2019-02-27 11:29:36 +0900

    Init as react-redux useContext

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,34 @@
-const root = document.querySelector(".root") as HTMLDivElement;
-root.textContent = "Hello, {app_name}";
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, Reducer } from "redux";
+import { ReactReduxContext, Provider } from "react-redux";
+
+type Counter = {
+  value: number;
+};
+type RootState = {
+  counter: Counter;
+};
+
+function counter(): Counter {
+  return { value: 0 };
+}
+
+const rootReducer: Reducer<RootState> = combineReducers({ counter });
+
+const store = createStore(rootReducer);
+
+function Counter() {
+  const { storeState } = useContext(ReactReduxContext);
+  return <div>value: {storeState.counter.value}</div>;
+}
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root") as HTMLDivElement);
